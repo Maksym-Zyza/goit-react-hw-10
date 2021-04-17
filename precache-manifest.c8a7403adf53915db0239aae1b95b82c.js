7b4738efc94af11a4fa4dc563c232d9ac86f4186
Maksym Zyza <mmziza88@gmail.com>
2021-04-17 22:09:43 +0300

    Updates

---
+++ precache-manifest.c8a7403adf53915db0239aae1b95b82c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f70f65d758caee8e8d39f30b739627",
+    "url": "/goit-react-hw-10/index.html"
+  },
+  {
+    "revision": "b9ac8e5dbaa1600a3fed",
+    "url": "/goit-react-hw-10/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "153079ff0947b4061fd6",
+    "url": "/goit-react-hw-10/static/css/3.f2f3e31f.chunk.css"
+  },
+  {
+    "revision": "82b40063a5b0ff96cc47",
+    "url": "/goit-react-hw-10/static/css/main.a29c3eb6.chunk.css"
+  },
+  {
+    "revision": "b9ac8e5dbaa1600a3fed",
+    "url": "/goit-react-hw-10/static/js/2.543b27b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-10/static/js/2.543b27b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153079ff0947b4061fd6",
+    "url": "/goit-react-hw-10/static/js/3.3dc9c8af.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/goit-react-hw-10/static/js/3.3dc9c8af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092280f4ecb5882856b8",
+    "url": "/goit-react-hw-10/static/js/4.00fcf878.chunk.js"
+  },
+  {
+    "revision": "92b7bc9ae5f7ea8260a3",
+    "url": "/goit-react-hw-10/static/js/5.6cc685bb.chunk.js"
+  },
+  {
+    "revision": "b8b9589bffbe7cf1f78d",
+    "url": "/goit-react-hw-10/static/js/6.f5fb44bc.chunk.js"
+  },
+  {
+    "revision": "82b40063a5b0ff96cc47",
+    "url": "/goit-react-hw-10/static/js/main.9d349ae0.chunk.js"
+  },
+  {
+    "revision": "8febe130dbe64265ed8f",
+    "url": "/goit-react-hw-10/static/js/runtime-main.36517a19.js"
+  },
+  {
+    "revision": "e3fa45cfb4e11b162b2b69ab85523972",
+    "url": "/goit-react-hw-10/static/media/add.e3fa45cf.svg"
+  },
+  {
+    "revision": "ccafcf3449f168838370d2463e7dc6ea",
+    "url": "/goit-react-hw-10/static/media/default-avatar.ccafcf34.png"
+  }
+]);
